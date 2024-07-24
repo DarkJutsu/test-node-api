29bab000ce7ab3497bcf3e5fe810a3eaed590b3c
DarkJutsu <ulloajose34@gmail.com>
2024-07-24 16:06:08 -0600

    feat(src/routes):Add rutas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { PORT } from "./config.js";
+import router from "./routes/users.routes.js";
 
 const app = express();
 
+app.use(router);
 app.listen(PORT);
 console.log("Server on port", PORT);
