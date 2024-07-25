5f76cd0334b34a5c90ba0b15376e5d7776a9f6bd
DarkJutsu <ulloajose34@gmail.com>
2024-07-24 21:56:01 -0600

    feat(src/routes):Add CRUD

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { json } from "express";
 import { PORT } from "./config.js";
 import router from "./routes/users.routes.js";
 
 const app = express();
 
+app.use(express.json());
 app.use(router);
+
 app.listen(PORT);
 console.log("Server on port", PORT);
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,27 +3,55 @@ import { pool } from "../db.js";
 
 const router = Router();
 
-router.get("/users", (req, res) => {
-  res.send("Obteniendo usuarios!!!");
+router.get("/users", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM users");
+  return res.json(rows);
 });
 
-router.get("/users/:id", (req, res) => {
+router.get("/users/:id", async (req, res) => {
   const { id } = req.params;
-  res.send("Obteniendo usuario " + id);
+
+  const { rows } = await pool.query("SELECT * FROM users WHERE id=$1", [id]);
+
+  if (rows.length === 0) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  return res.json(rows[0]);
 });
 
-router.post("/users", (req, res) => {
-  res.send("Creando usuario!!!");
+router.post("/users", async (req, res) => {
+  const data = req.body;
+  const { rows } = await pool.query(
+    "INSERT INTO users(name, email) VALUES($1, $2) RETURNING *",
+    [data.name, data.email]
+  );
+  return res.json(rows[0]);
 });
 
-router.delete("/users/:id", (req, res) => {
+router.delete("/users/:id", async (req, res) => {
   const { id } = req.params;
-  res.send("Eliminando usuario " + id);
+
+  const { rowCount } = await pool.query(
+    "DELETE FROM users WHERE id=$1 RETURNING *",
+    [id]
+  );
+
+  if (rowCount === 0) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  return res.sendStatus(204);
 });
 
-router.put("/users/:id", (req, res) => {
+router.put("/users/:id", async (req, res) => {
   const { id } = req.params;
-  res.send("Actualizando usuario " + id);
+  const data = req.body;
+
+  const { rows } = await pool.query(
+    "UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *",
+    [data.name, data.email, id]
+  );
+
+  return res.json(rows[0]);
 });
 
 export default router;
