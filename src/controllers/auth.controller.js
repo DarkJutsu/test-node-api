48e4136a18f9bcde2c34c34d1a60b7c9f031245e
DarkJutsu <ulloajose34@gmail.com>
2024-07-27 19:39:47 -0600

    feat(src/):Add auth routes & controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,24 @@
 import { Validations } from "../utils/Validations.js";
 import { getUserByUsername } from "./users.controller.js";
+import bcrypt from "bcrypt";
 
 export const login = async (req, res) => {
   try {
-    const data = req.body;
-    Validations.username(data.user);
-    Validations.password(data.password);
+    const { user, password } = req.body;
+    Validations.username(user);
+    Validations.password(password);
 
-    const user = await getUserByUsername(data.user);
-    return res.json(user);
+    const getUser = await getUserByUsername(user);
+
+    const isValid = bcrypt.compareSync(password, getUser.password);
+    if (!isValid) throw new Error("Password is invalid!!!");
+
+    const publicUser = {
+      id: getUser.id,
+      username: getUser.user_name,
+    };
+
+    return res.json(publicUser);
   } catch (err) {
     return res.send(err.message);
   }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,9 @@ import { login } from "../controllers/auth.controller.js";
 
 const routerAuth = Router();
 
+routerAuth.get("/", (req, res) => {
+  res.render("index", { username: "Samllo 95" });
+});
 routerAuth.post("/login", login);
 
 export default routerAuth;
