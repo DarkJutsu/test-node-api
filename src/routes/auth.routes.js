7e536601c7f18a3c020e2f8611f251b7442bd63f
DarkJutsu <ulloajose34@gmail.com>
2024-08-05 17:37:36 -0600

    feat(src/auth.routes):Add routes register & protected

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { login } from "../controllers/auth.controller.js";
+import { createUser } from "../controllers/users.controller.js";
 
 const routerAuth = Router();
 
 routerAuth.get("/", (req, res) => {
-  res.render("index", { username: "Samllo 95" });
+  res.render("index");
 });
 routerAuth.post("/login", login);
+routerAuth.post("/register", createUser);
+routerAuth.get("/protected", (req, res) => {
+  res.render("protected");
+});
 
 export default routerAuth;
