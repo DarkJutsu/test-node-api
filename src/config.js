d51fb48acfa7e6294e0541284b2115ca85268ac5
DarkJutsu <ulloajose34@gmail.com>
2024-08-05 18:12:58 -0600

    feat(src/config):Add KEY-SECRET & refactored code

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
-export const PORT = process.env.PORT || 4400;
-
-export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_DATABASE = process.env.DB_DATABASE;
-export const DB_PORT = process.env.DB_PORT;
-
-export const SALT_ROUNDS = 10;
+export const {
+  PORT = process.env.PORT || 4400,
+  DB_USER = process.env.DB_USER,
+  DB_PASSWORD = process.env.DB_PASSWORD,
+  DB_HOST = process.env.DB_HOST,
+  DB_DATABASE = process.env.DB_DATABASE,
+  DB_PORT = process.env.DB_PORT,
+  SALT_ROUNDS = 10,
+  SECRET_JWT_KEY = process.env.SECRET_JWT_KEY,
+} = process.env;
